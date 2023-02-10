b5c4cd0c13b0fb82485c55a1f71fc647b5853d26
SirDomin <dominik.garbulski@gmail.com>
2023-02-10 21:38:10 +0100

    add summoner provider enpoints

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\ApiManager\LeagueApi;
 use App\Provider\GameProvider;
+use App\Provider\SummonerDataProvider;
+use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializerBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,6 +17,9 @@ class GameController extends AbstractController
 {
     public function __construct(
         private readonly GameProvider $gameProvider,
+        private readonly LeagueApi $leagueApi,
+        private readonly GameRepository $gameRepository,
+        private readonly EntityManagerInterface $entityManager,
     ) { }
 
     #[Route('/game', name: 'game')]
@@ -25,7 +32,7 @@ class GameController extends AbstractController
         return new JsonResponse(['created' => $game]);
     }
 
-    #[Route('/game/{puuid}', name: 'game-show', methods: ['GET'])]
+    #[Route('/game/by-puuid/{puuid}', name: 'game-show', methods: ['GET'])]
     public function show(string $puuid): Response
     {
         $serializer = SerializerBuilder::create()->build();
@@ -33,5 +40,54 @@ class GameController extends AbstractController
         $game = $this->gameProvider->provideGameByMatchId($puuid);
 
         return new Response($serializer->serialize($game, 'json'));
+    }
+
+    #[Route('/game/active/{summonerName}', name: 'game-find-active', methods: ['GET'])]
+    public function findActive(string $summonerName): Response
+    {
+        $serializer = SerializerBuilder::create()->build();
+
+        $game = $this->gameProvider->provideActiveGameForUser($summonerName);
+
+//        $g = "{\"info\":[{\"games_played\":1,\"summoner_id\":\"av5y7ed_g9eI_wx_Rl6C9bx5xfM2q8xzMvlhXQ5fOpqln4c\",\"summoner_name\":\"FakersMan\",\"team_id\":100,\"champion_id\":54},{\"games_played\":0,\"summoner_id\":\"s4HZXSmKodBRQ1BS5UQqJXQDDNy-Y2VN2X-ivy4kqC13JiE\",\"summoner_name\":\"WonderBoiTTT\",\"team_id\":100,\"champion_id\":80},{\"games_played\":0,\"summoner_id\":\"fWgPMFC8Y18AIAEbPfaXJnZC3n9jPNbuGsdxecFWMznYUyI\",\"summoner_name\":\"AADiN\",\"team_id\":100,\"champion_id\":51},{\"games_played\":0,\"summoner_id\":\"jaxFeP7I91zeKieYZmWKa3XdZ6Zx88CBrbD6Eo9EOrV1xpQ\",\"summoner_name\":\"Marcher\",\"team_id\":100,\"champion_id\":141},{\"games_played\":0,\"summoner_id\":\"G5Hj7bZvvJ6v6UBBdBRXG_mLhKrkszx6fHB7jcMHUl6eOgc\",\"summoner_name\":\"BornToBonk\",\"team_id\":100,\"champion_id\":90},{\"games_played\":0,\"summoner_id\":\"pr67AKdqhSGzOTGgsW_cRSt5EommNLzgBIWRhQkBR8_fwio\",\"summoner_name\":\"AngeLinHell\",\"team_id\":200,\"champion_id\":79},{\"games_played\":0,\"summoner_id\":\"23EegUJ-A3k20A_O2NlZ-TKOKbx7EvIQn_cytBxfap-4Yfk\",\"summoner_name\":\"Terror9611\",\"team_id\":200,\"champion_id\":74},{\"games_played\":0,\"summoner_id\":\"N7g5cen1pEHQejPVcvh8RfwjBeUAmBn99Fas0W4FwR-1GkU\",\"summoner_name\":\"Helloo\",\"team_id\":200,\"champion_id\":58},{\"games_played\":1018,\"summoner_id\":\"H4oxX_PTSb6jYdnK5Nj6QwjIrPMzvAEIcK9LTtfawoOTb14\",\"summoner_name\":\"SirDomin\",\"team_id\":200,\"champion_id\":121},{\"games_played\":215,\"summoner_id\":\"UrnLhXmr-nJPoW8sH9o4PuUhrwSrbmL3AR1VENKRLMvrG4M\",\"summoner_name\":\"MamSyndrąDowna\",\"team_id\":200,\"champion_id\":76}]}";
+//        return new Response($g);
+        return new Response($serializer->serialize(['info' => $game], 'json'));
+    }
+
+    #[Route('/game/save', name: 'game-save', methods: ['GET'])]
+    public function saveGame(): Response
+    {
+        $gameIds = $this->leagueApi->getGamesHistory('SirDomin', 1, 0);
+
+        $game = $this->gameProvider->provideGameByMatchId($gameIds[0]);
+
+        $serializer = SerializerBuilder::create()->build();
+
+        if($game->getId() === null) {
+            $this->entityManager->persist($game);
+            $this->entityManager->flush();
+        }
+
+        return new Response($serializer->serialize(
+            [
+                'game' => $game
+            ],
+            'json')
+        );
+    }
+
+    #[Route('/game/last', name: 'game-get-last', methods: ['GET'])]
+    public function getLast(): Response
+    {
+        $serializer = SerializerBuilder::create()->build();
+
+        $game = $this->gameRepository->getLastGame();
+
+        return new Response($serializer->serialize(
+            [
+                'game' => $game
+            ],
+            'json')
+        );
     }
 }
--- src/Controller/SummonerController.php
+++ src/Controller/SummonerController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Provider\GameProvider;
 use App\Provider\SummonerDataProvider;
 use App\Repository\GameRepository;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializerBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,17 +20,42 @@ class SummonerController extends AbstractController
     public function __construct(
         private readonly GameRepository $gameRepository,
         private readonly SummonerDataProvider $summonerDataProvider,
+        private readonly ParticipantRepository $participantRepository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly GameProvider $gameProvider,
     ){ }
 
-    #[Route('/summoner/{summonerName}', name: 'summoner-show', methods: ['GET'])]
-    public function show(string $summonerName): Response
+    #[Route('/summoner/{summonerId}', name: 'summoner-show', methods: ['GET'])]
+    public function show(string $summonerId): Response
     {
         $serializer = SerializerBuilder::create()->build();
 
-        $summonerUuid = $this->summonerDataProvider->getPuuidByName($summonerName);
-        $games = $this->gameRepository->getAllGamesWithPlayer($summonerUuid);
+        $games = $this->gameRepository->getAllGamesWithPlayerBySummonerId($summonerId);
 
         return new Response($serializer->serialize($games, 'json'));
+    }
+
+    #[Route('/summoners/championSelect', name: 'champion-select', methods: ['POST'])]
+    public function showChampionSelect(Request $request): Response
+    {
+        $serializer = SerializerBuilder::create()->build();
+
+        $content = json_decode($request->getContent(), true);
+
+        $participantsData = [];
+
+        foreach ($content as $data) {
+            $summonerData = $this->summonerDataProvider->getDataByName($data['summonerName']);
+
+            $participantsData[] = [
+                  'summonerId' => $summonerData['id'],
+                  'summonerName' => $summonerData['name'],
+                  'teamId' => null,
+                  'championId' => null,
+              ];
+        }
+
+        return new Response($serializer->serialize(['info' => $this->gameProvider->connectParticipants(['participants' => $participantsData])], 'json'));
     }
 
     #[Route('/summoner/{summonerName}/count', name: 'summoner-count', methods: ['GET'])]
@@ -35,5 +66,23 @@ class SummonerController extends AbstractController
         $summonerUuid = $this->summonerDataProvider->getPuuidByName($summonerName);
 
         return new Response($serializer->serialize(['games' => $this->gameRepository->countAllGamesWithPlayer($summonerUuid)], 'json'));
+    }
+
+    #[Route('/game/save-result', name: 'summoner-update', methods: ['POST'])]
+    public function edit(Request $request): Response
+    {
+        $content = json_decode($request->getContent(), true);
+
+        foreach ($content as $data) {
+            /** @var Participant $summoner */
+            $summoner = $this->participantRepository->findOneBy(['id' => (int) $data['id']]);
+
+            $summoner->setComment($data['comment']);
+            $this->entityManager->persist($summoner);
+        }
+
+        $this->entityManager->flush();
+
+        return new Response();
     }
 }
